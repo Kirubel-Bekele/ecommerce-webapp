3f397275fb9135e7247f8d31b22eacf0334d63aa
QhF@Qb8A64Yg5m <kirubeltaye1991@gmail.com>
2021-12-10 10:23:10 +0300

    store and carts functionality

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
     path('store/',include('store.url')),
+    path('cart/',include('carts.url'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
